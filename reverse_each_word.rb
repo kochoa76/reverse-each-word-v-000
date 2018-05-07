1d0bbf6619642bc598aa74274bad22cf71740312
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-07 22:12:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,13 @@
 def reverse_each_word(sentence)
+  sentence.split.collect {|word|
+    word.reverse}.join(" ")
+
+  
 reversed_sentence= []
 sentence.split.each do |word|
 reversed_sentence<< word.reverse
 end
 return reversed_sentence.join(" ")
 
-sentence.split.collect {|word|
-  word.reverse}.join(" ")
+
 end
