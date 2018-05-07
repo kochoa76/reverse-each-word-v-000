2460241040ff9bbb63a6d658dafbe6f55540be19
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-07 22:12:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word|
     word.reverse}.join(" ")
 
-  
+
 reversed_sentence= []
 sentence.split.each do |word|
 reversed_sentence<< word.reverse
